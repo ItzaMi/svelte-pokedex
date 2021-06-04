fa3edc106239a367bf73b51c5ba38d37faa4c79e
ItzaMi <rros00@gmail.com>
2021-06-04 20:48:09 +0100

    [PokemonBanner] component to displace images

---
+++ src/components/PokemonBanner.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let pokemonName;
+  export let pokemonFrontImage;
+  export let pokemonBackImage;
+  export let numberOfRepetitions;
+</script>
+
+{#each Array(numberOfRepetitions) as _, index}
+  <img src={pokemonFrontImage} alt={pokemonName} class="w-10" key={index} />
+  <img src={pokemonBackImage} alt={pokemonName} class="w-10" key={index} />
+{/each}
